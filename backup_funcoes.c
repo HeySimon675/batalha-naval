f74aa55f15dbad25e8f891371acb6cceda2171e9
gabriel simão <gabrielsimao@hotmail.com>
2018-11-11 17:55:07 +0000

    Update backup_funcoes.c

--- backup_funcoes.c
+++ backup_funcoes.c
@@ -12,3 +12,21 @@ void Submarino(char m[20][20], int n, char id) {
         }
     }
 }
+
+
+void Porta_Avioes(char m[20][20], int n) {
+    int pos, i, erro = 0;
+    char id = '5';
+    int largura = 5;
+    int altura = 2;
+    for (i = 0; i < n; i++) {
+
+        erro=0;
+        pos = rand() % 2;
+        if (pos)
+            Posicao(&largura,&altura);
+        while (!erro) {     //Continua até conseguir posicionar a embarcação
+            erro = Posicionadora(m, largura, altura, id);
+        }
+    }
+}
