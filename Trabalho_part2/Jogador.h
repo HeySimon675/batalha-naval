489ecb9e26bda94bdbeac66f19d8f79bd040a779
gabriel simão <gabrielsimao@hotmail.com>
2018-11-21 18:23:24 +0000

    Upload New File

---
+++ Trabalho_part2/Jogador.h
@@ -0,0 +1,21 @@
+//
+// Created by root on 21/11/18.
+//
+
+#ifndef CLION_JOGADOR_H
+#define CLION_JOGADOR_H
+
+#include "Tabuleiro.h"
+
+typedef struct {
+    int id;
+    int vencedor;
+    int frota_total;
+    Tabuleiro tab;
+}Jogador;
+
+Jogador* CadastraJogador(int id);
+int FrotaAtual (Jogador* player);
+
+
+#endif //CLION_JOGADOR_H
