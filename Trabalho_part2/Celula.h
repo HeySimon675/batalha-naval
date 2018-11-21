433bb972da17f7ea2ac2b924bd45ac59e844090c
gabriel simão <gabrielsimao@hotmail.com>
2018-11-21 18:22:32 +0000

    Upload New File

---
+++ Trabalho_part2/Celula.h
@@ -0,0 +1,17 @@
+//
+// Created by root on 21/11/18.
+//
+
+#ifndef CLION_CELULA_H
+#define CLION_CELULA_H
+
+typedef struct {
+    int visivel;
+    char valor;
+}Celula;
+
+Celula* CriaCelula();
+void DestroiCelula(Celula* campo);
+char Tiro(Celula* campo);
+
+#endif //CLION_CELULA_H
