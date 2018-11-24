f1d339f6dc5bda3f014995d5f71618220a149596
gabriel simão <gabrielsimao@hotmail.com>
2018-11-24 14:27:17 +0000

    Replace Jogador.c

--- Trabalho_part2/Jogador.c
+++ Trabalho_part2/Jogador.c
@@ -1,5 +1,16 @@
 //
 // Created by root on 21/11/18.
 //
-
+#include <stdlib.h>
 #include "Jogador.h"
+
+Jogador* CadastraJogador(int id,char* tab){
+    Jogador* player = (Jogador*) malloc (sizeof(Jogador));
+    player->id= id;
+    player->vencedor=0;
+    player->frota_total = 30;
+    player->tab = CriaTabuleiro(tab);
+}
+int FrotaAtual (Jogador* player){
+
+}
