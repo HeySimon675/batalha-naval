772f87057ce9a12b9b1d18b1ddaf1c80e72fff51
gabriel simão <gabrielsimao@hotmail.com>
2018-11-24 14:28:15 +0000

    Replace main.c

--- Trabalho_part2/main.c
+++ Trabalho_part2/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Tabuleiro.h"
+#include "Gtabuleiro.h"
+#include "Game.h"
+
+#define TAB_PLAYER "tabuleiro1.txt"
+#define TAB_MAQUINA "tabuleiro2.txt"
+
+#define  PLAYER_ID 0
+#define  MAQUINA_ID 1
+
+#include "Game.h"
 
 
 int main() {
 
-    FILE* tabuleiro1 = CriaTabuleiro;
-    fclose(tabuleiro1);
+    Game* jogo = NovoGame(TAB_PLAYER,TAB_MAQUINA);
+
+    do{
+
+    }while (!jogo->fim_de_jogo);
+        AtirarPlayer(jogo,PLAYER_ID);
+        AtirarPlayer(jogo,MAQUINA_ID);
+
+    //FILE* tabuleiro1 = CriaTabuleiro;
+    //fclose(tabuleiro1);
     return 0;
 }
