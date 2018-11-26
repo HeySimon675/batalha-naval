00abd935b3a1d4870d0085268624b22ff647d171
gabriel simão <gabrielsimao@hotmail.com>
2018-11-26 00:02:19 +0000

    Replace Tabuleiro.c

--- Trabalho_part2/Tabuleiro.c
+++ Trabalho_part2/Tabuleiro.c
@@ -5,26 +5,77 @@
 #include "Tabuleiro.h"
 #define ROW 20
 #define COW 20
-extern int i_inicial, i_final, j_inicial, j_final;
+#define NORTE 0
+#define SUL 1
+#define LESTE 2
+#define OESTE 3
 
 void IncluiBomba (Tabuleiro* tab,int i, int j) {
     tab->matriz_campo[i][j].valor='0';
 }
 void IncluiAviao (Tabuleiro* tab,int i, int j, int pos){
-    extern int i_inicial, i_final, j_inicial, j_final;
+    int x, y;
     tab->matriz_campo[i][j].inicial=1;
     switch (pos){
-        case 0:
-            j_inicial=j-1;
-            i_inicial=i-1;
-            i_final=i_inicial+3;
-            j_final=j_inicial+2;
+        case LESTE: //LESTE
+            y=j;
+            for(x=i-1; x<(i+2) ; x++){  //(x+2) esta definindo o limite de altura do aviao
+                tab->matriz_campo[x][y].valor='1'; // 1 é o id do aviao
+            }
+            tab->matriz_campo[i][j-1].valor='1'; //coloca a cauda do avião, que esta à esquerda da posição inicial
+            break;
+        case OESTE: // OESTE
+            y=j;
+            for(x=i-1; x<(i+2) ; x++){  //(x+2) esta definindo o limite de altura do aviao
+                tab->matriz_campo[x][y].valor='1'; // 1 é o id do aviao
+            }
+            tab->matriz_campo[i][j+1].valor='1'; //coloca a cauda do avião, que esta à direita da posição inicial
+            break;
+
+        case NORTE: //NORTE
+            x=i;
+            for(y=(j-1);y<(j+2);y++){
+                tab->matriz_campo[x][y].valor='1'; // 1 é o id do aviao
+            }
+            tab->matriz_campo[i+1][j].valor='1'; //coloca a cauda do avião, que esta à abaixo da posição inicial
+            break;
+
+        case SUL: //SUL
+            x=i;
+            for(y=(j-1);y<(j+2);y++){
+                tab->matriz_campo[x][y].valor='1'; // 1 é o id do aviao
+            }
+            tab->matriz_campo[i-1][j].valor='1'; //coloca a cauda do avião, que esta à acima da posição inicial
             break;
     }
 }
 
 void IncluiSubmarino (Tabuleiro* tab, int i, int j, int pos){
+    int x, y ;
+    tab->matriz_campo[i][j].inicial=1;
+    switch (pos){
+        case LESTE: //LESTE
+            for(y=j; y<(j+4) ; y++){  //(y+4) esta definindo o limite de largura do submarino
+                tab->matriz_campo[i][y].valor='2'; // 2 é o id do submarino
+            }
+            break;
+        case OESTE: // OESTE
+            for(y=(j-3); y<(j+1) ; y++){  //(j-3) pois o ponto inicial é na ponta direita, (y+1) esta definindo o limite de largura do submarino
+                tab->matriz_campo[i][y].valor='2'; // 2 é o id do submarino
+            }
+            break;
 
+        case NORTE: //NORTE
+            for(x=i; x<(i+4) ; x++){  //(i+4) esta definindo o limite de altura do submarino
+                tab->matriz_campo[x][j].valor='2'; // 2 é o id do submarino
+            }
+            break;
+        case SUL: //SUL
+            for(x=(i-3); x<(i+1) ; x++){  //(i-3) pois o ponto inicial é na ponta embaixo, (x+1) esta definindo o limite de altura do submarino
+                tab->matriz_campo[x][j].valor='2'; // 2 é o id do submarino
+            }
+            break;
+    }
 }
 
 void IncluiEspiao (Tabuleiro* tab, int i, int j, int pos,char id){
@@ -37,18 +88,18 @@ void IncluiPAviao (Tabuleiro* tab, int i, int j, int pos){
 }
 
 void IncluiEmbarc (Tabuleiro* tab, FILE* tabfile){
-    char n,id,pos,i[2],j[2];
-    while (fscanf(tabfile,"%[^ ] %[^ ] %[^ ] %[^ ] %[^\n]\n",&n,&id,&pos,i,j) != EOF){
+    char n,id,pos[2],i[2],j[2];
+    while (fscanf(tabfile,"%[^ ] %[^ ] %[^ ] %[^ ] %[^\n]\n",&n,&id,pos,i,j) != EOF){
         if(id=='0') {
             IncluiBomba(tab, (atoi(i)), atoi(j));
         }else if (id=='1'){
-            IncluiAviao(tab,atoi(i),atoi(j),atoi(&pos));
+            IncluiAviao(tab,atoi(i),atoi(j),atoi(pos));
         }else if (id=='2'){
-            IncluiSubmarino(tab,atoi(i),atoi(j),atoi(&pos));
+            IncluiSubmarino(tab,atoi(i),atoi(j),atoi(pos));
         }else if (id=='3' || id=='4'){
-            IncluiEspiao(tab, atoi(i),atoi(j),atoi(&pos),id);
+            IncluiEspiao(tab, atoi(i),atoi(j),atoi(pos),id);
         }else
-            IncluiPAviao(tab,atoi(i),atoi(j),atoi(&pos));
+            IncluiPAviao(tab,atoi(i),atoi(j),atoi(pos));
     }
 }
 
