37e798b12e79f68d08337888a2dcb67205abd163
gabriel simão <gabrielsimao@hotmail.com>
2018-12-02 04:08:43 +0000

    Replace Tabuleiro.h

--- Trabalho_part2/Tabuleiro.h
+++ Trabalho_part2/Tabuleiro.h
@@ -7,31 +7,35 @@
 
 #include "Celula.h"
 #include "Gtabuleiro.h"
+#include "barcos.h"
 #include <stdio.h>
-//#include "Embarc.h"
+
 
 typedef struct {
     int linhas;
     int colunas;
     Celula** matriz_campo;
+    Barcos* barcos;
+
 }Tabuleiro;
 
 Tabuleiro* CriaTabuleiro(char* txt);
 void IncluiEmbarc (Tabuleiro* tab, FILE* tabfile);
 void DestroiTabuleiro(Tabuleiro* tab);
 
-
-//FUNÇÕES DA BIBLIOTECA EMBARC, QUE POR ALGUM CARALHO DE MOTIVO NAO TA FUNCIONANDO
-
-void IncluiBomba (Tabuleiro* tab,int i, int j);
+void IncluiBomba (Tabuleiro* tab,int i, int j, char n);
 
 void IncluiAviao (Tabuleiro* tab,int i  , int j, int pos);
 
-void IncluiSubmarino (Tabuleiro* tab, int i, int j, int pos);
+void IncluiSubmarino (Tabuleiro* tab, int i, int j, char* pos);
 
 void IncluiEspiao (Tabuleiro* tab, int i, int j, int pos,char id);
 
 void IncluiPAviao (Tabuleiro* tab, int i, int j, int pos);
+
+void PrintaTabNovo (Tabuleiro *tab);
+
+void PrintaTabuleiro (Celula** matriz, int row);
 
 
 
